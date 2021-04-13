fa3fa091b8769c20b69945b2f300551d37eeb3d5
Andrzej Janaszek <janaszek.andrzej.szkola@gmail.com>
2021-04-13 14:11:17 +0200

    Bazowe pliki status rozwoju - 0

---
+++ footer.php
@@ -0,0 +1,8 @@
+<?php
+    $footerHTML='
+    <footer>
+        Tomasz Jamróz Andrzej Janaszek Mikołaj Kozioł Krzysztof Marx &copy;
+    </footer>
+    ';
+    echo $footerHTML;
+?>
---
+++ header.php
@@ -0,0 +1,7 @@
+<?php
+    $headerHTML='
+    <header>
+    
+    </header>
+    ';
+?>
